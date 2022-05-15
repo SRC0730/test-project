768d2a953399396522dcfd8fc4f5ac29fa7c8e7d
SRC0730 <davemark0730@hotmail.com>
2022-05-15 09:17:47 +0800

    two site & one lib

---
+++ projects/tools/src/lib/tools.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tools-tools',
+  template: `
+    <p>
+      tools works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ToolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
